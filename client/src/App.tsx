b998728a60ac31d5b4f742685beb6b3cc9385e8b
SnegurkaWs <snegurka@web-esse.ru>
2022-07-19 19:38:51 +0500

    ADD: base routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,22 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./page/Home/Home";
+import List from "./page/List/List";
+import Login from "./page/Login/Login";
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" />
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="project">
+            <Route index element={<List />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
