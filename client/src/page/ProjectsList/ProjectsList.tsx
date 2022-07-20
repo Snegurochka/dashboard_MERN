33a11ce263a9ae2b037d6b7c6ee9f4732f5123f3
SnegurkaWs <snegurka@web-esse.ru>
2022-07-20 21:24:55 +0500

    ADD: gql query for projects

--- client/src/page/ProjectsList/ProjectsList.tsx
+++ client/src/page/ProjectsList/ProjectsList.tsx
@@ -1,43 +1,31 @@
+import { FC } from "react";
 
 import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import { useQuery } from "@apollo/client";
+import { ProjectsAPIType } from "../../types/APITypes";
+import { GET_PROJECTS } from "../../queries/projectQueries";
 
 const columns: GridColDef[] = [
   { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "First name", width: 130 },
-  { field: "lastName", headerName: "Last name", width: 130 },
+  { field: "namee", headerName: "Name", width: 130 },
+  { field: "description", headerName: "Description", width: 130 },
   {
-    field: "age",
-    headerName: "Age",
-    type: "number",
+    field: "status",
+    headerName: "Status",
     width: 90,
   },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (params: GridValueGetterParams) =>
-      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-  },
 ];
 
-const projects = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-];
+const ProjectsList: FC = () => {
+  const { loading, error, data } = useQuery<ProjectsAPIType>(GET_PROJECTS);
 
-const ProjectsList = () => {
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Eror</p>;
+  if (!data) return <p>No projects</p>;
+
   return (
     <DataGrid
-      rows={projects}
+      rows={data.projects}
       columns={columns}
       pageSize={5}
       rowsPerPageOptions={[5]}
