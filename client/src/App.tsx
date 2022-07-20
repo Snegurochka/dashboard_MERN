16d400e07b3ff0d08bedad057d40c6149f2bd4fb
SnegurkaWs <snegurka@web-esse.ru>
2022-07-20 14:14:14 +0500

    ADD: projects list page with mock data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
-import { Container, Grid } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.scss";
+
+import { Grid } from "@mui/material";
+
+// Components
 import Navbar from "./components/Layout/Navbar/Navbar";
 import Sidebar from "./components/Layout/Sidebar/Sidebar";
 import Home from "./page/Home/Home";
-import List from "./page/List/List";
 import Login from "./page/Login/Login";
+import ProjectsList from "./page/ProjectsList/ProjectsList";
+
 
 function App() {
   return (
@@ -19,8 +22,8 @@ function App() {
             <Route path="/" />
             <Route index element={<Home />} />
             <Route path="login" element={<Login />} />
-            <Route path="project">
-              <Route index element={<List />} />
+            <Route path="projects">
+              <Route index element={<ProjectsList />} />
             </Route>
           </Routes>
         </Grid>
